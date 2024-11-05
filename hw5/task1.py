a07687cd5431340bb241ba3dafeb7d944acaa170
Maria <e_maria03@mail.ru>
2024-11-06 00:51:23 +0300

    hw5

---
+++ hw5/task1.py
@@ -0,0 +1,13 @@
+# Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
+# Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+
+def tuple_file(inp_str):
+    *a, b, c = inp_str.split('/')
+    name_file, file_extension = c.split('.')
+    res = (inp_str, name_file, file_extension)
+    return res
+
+
+inp_str = 'D:/Data/MyFiles/ile.png'
+print(tuple_file(inp_str))
