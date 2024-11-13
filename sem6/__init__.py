cce0575d4fd6d962ea24c5a18af09004e3354664
Maria <e_maria03@mail.ru>
2024-11-13 15:22:37 +0300

    sem6

---
+++ sem6/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['check_date', 'guess_num', 'guess_pazzl']
---
+++ sem6/task1.py
@@ -0,0 +1,10 @@
+# Создайте файл, в котором вы импортируете встроенные в
+# модуль функции под псевдонимами. (3-7 строк импорта).
+
+from random import randint as rnd
+from math import sqrt
+import decimal
+import sys
+import time
+import fractions
+
